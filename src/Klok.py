f016c8bf87f9966cac28244dd648e9518d37ac08
FunMetJoel <101552207+FunMetJoel@users.noreply.github.com>
2023-07-18 09:32:54 +0200

    Now using time.ticksms for better presision and to big numbers

--- src/Klok.py
+++ src/Klok.py
@@ -1,30 +1,40 @@
 # Imports go at the top
 from microbit import *
 import neopixel
-import random
+import time
 
 numLeds = 24
 np = neopixel.NeoPixel(pin0, numLeds)
 Truecolor = (255, 0, 0)
 Falsecolor = (0, 0, 255)
 
+def TimeToMs(uur, minuten, seconden):
+    sec = 0
+    sec += uur * 3600
+    sec += minuten * 60
+    sec += seconden
+    ms = sec * 1000
+    return ms
+
 tijden = [
-    0,
-    45
+    TimeToMs(0, 0, 0),
+    TimeToMs(0, 0, 10),
+    TimeToMs(8, 10, 0),
+    TimeToMs(8, 55, 0),
+    TimeToMs(9, 40, 0)
 ]
 
-t = 720;
-
-def RoundTime():
+def TimeFraction(t):
     for i in range(0, len(tijden)):
-        if tijden[i] <= t:
-            return float((t - tijden[i])/(tijden[i+1] - tijden[i]))
-
+        if time.ticks_diff(currTime, tijden[i]) < 0:
+            timeSinceBel = time.ticks_diff(tijden[i-1], currTime)
+            lenghtOfLesson = time.ticks_diff(tijden[i-1], tijden[i])
+            return float(timeSinceBel/lenghtOfLesson)
     return float(0)
 
-def UpdateLeds():
+def UpdateLeds(t):
     for i in range(0, numLeds):
-        if (RoundTime() >= float(i/numLeds)):
+        if (TimeFraction(t) >= float(i/numLeds)):
             np[i] = Truecolor
         else:
             np[i] = Falsecolor
@@ -33,6 +43,6 @@ def UpdateLeds():
 
 # Code in a 'while True:' loop repeats forever
 while True:
-    UpdateLeds()
-    t = t + 1
-    sleep(1000)
+    currTime = time.ticks_ms()
+    print(TimeFraction(currTime))
+    UpdateLeds(currTime)
