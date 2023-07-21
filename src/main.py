bd0c30a09bbe15a3eda064198cabec6705820c36
FunMetJoel <101552207+FunMetJoel@users.noreply.github.com>
2023-07-21 17:37:35 +0200

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,15 @@
+# Imports go at the top
+from microbit import *
+import time
+from Clock import *
+
+cl = Clock(24, 0.05)
+# Code in a 'while True:' loop repeats forever
+while True:
+    currTime = time.ticks_ms()
+    #cl.ShowProgressBar(TimeFraction(currTime), Truecolor, Falsecolor)
+    cl.ShowRadarfade(TimeFraction(currTime), Truecolor, Falsecolor)
+    #cl.ShowColorfade(TimeFraction(currTime), [(255,0,0),(0,255,0),(0,0,255)])
+    #cl.ShowStaticColorfade([(255,0,0),(0,255,0),(0,0,255),(255,0,0),(0,255,0),(0,0,255)])
+
+        
