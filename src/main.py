a0f61ad42beb2b86a9a38c7f1d9266b434d98992
FunMetJoel <101552207+FunMetJoel@users.noreply.github.com>
2023-08-22 18:29:11 +0200

    Je kunt nu kleurtje aanpassen enz

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ lt = 0.0
 colorScheme = [(0,137,133),(239,121,17)]
 
 state = 0 #0: lost Energy, 1: calibrated, 2: settings
-subState = 0
+subState = 1
 
 radio.config(group=177)
 radio.on()
@@ -20,11 +20,22 @@ radio.on()
 while True:
     message = radio.receive()
     if message:
-        display.scroll(message)
+        #Format{Clr:255,255,255,000,000,000} 
+        if (message[0:3] == "Clr"):
+            print(message)
+            print(message[4:7])
+            print(message[8:11])
+            print(message[12:15])
+            print(message[16:19])
+            print(message[20:23])
+            print(message[24:27])
+            colorScheme[0] = (int(message[4:7]),int(message[8:11]),int(message[12:15]))
+            colorScheme[1] = (int(message[16:19]),int(message[20:23]),int(message[24:27]))
+
     
     if (state == 0):
         music.play(music.BA_DING)
-        state = 2
+        state = 1
     elif(state == 1):
         if(subState == 0):
             cl.ShowProgressBar(LoopTimeFraction(3000), colorScheme[0], colorScheme[1])
@@ -39,12 +50,15 @@ while True:
         elif(subState == 5):
             cl.RandomLeds(LoopTimeFraction(250), 0.99)
         else:
-            cl.ShowStaticColorfade([(255,0,0)])
+            cl.ShowColorfade(LoopTimeFraction(3000), [(255,0,0),(0,0,0)])
     elif(state == 2):
         if(subState == 0):
-            cl.ShowStaticColorfade([colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[1],colorScheme[1],colorScheme[1],colorScheme[1],colorScheme[1],colorScheme[1]])
+            cl.ShowColorfade(LoopTimeFraction(3000), [colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[0],(0,0,0),colorScheme[1],colorScheme[1],colorScheme[1],colorScheme[1],colorScheme[1],colorScheme[1],(0,0,0)])
         elif(subState == 1):
             pass
+        else:
+            cl.ShowColorfade(LoopTimeFraction(3000), [(255,0,0),(0,0,0)])
+
             
 
         
