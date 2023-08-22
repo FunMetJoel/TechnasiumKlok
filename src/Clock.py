7929c8ae71f31d0812789c1ce2b3f1038f635694
FunMetJoel <101552207+FunMetJoel@users.noreply.github.com>
2023-08-22 15:13:20 +0200

    Settings begin

--- src/Clock.py
+++ src/Clock.py
@@ -6,22 +6,21 @@ import math
 import Colors
 import random
 
-Truecolor = (0,137,133)
-Falsecolor = (239,121,17)
-
 class Clock:
     def __init__(self, _numLeds, _brightness):
         self.numLeds = _numLeds
         self.brightness = _brightness
+        self.pixels = [clPixel() for i in range(0, self.numLeds)]
         self.np = neopixel.NeoPixel(pin0, self.numLeds)
         self.ledFraction = 1/self.numLeds
 
-    def RandomLeds(self):
-        self.setLed(random.randint(0, self.numLeds),color=(255, 255, 255) ,brightness=1)
+    def RandomLeds(self, t, fadeMultiplyer):
+        if(t < 0.02):
+            self.setLed(random.randint(0, self.numLeds-1),color=(255, 255, 255) ,brightness=1)  
+        
         for i in range(0, self.numLeds):
-            print(self.np[i])
-            self.setLed(i, self.np[i], 0.75)
-        self.np.show()
+            self.pixels[i].A = self.pixels[i].A * fadeMultiplyer
+        self.show()
     
     def ShowProgressBar(self, t, Truecolor, Falsecolor):
         fullLeds = math.floor(t/self.ledFraction)
@@ -35,7 +34,7 @@ class Clock:
             else:
                 self.setLed(i,Falsecolor)
 
-        self.np.show()
+        self.show()
 
     def ShowRadarfade(self, t, Truecolor, Falsecolor):
         
@@ -47,20 +46,38 @@ class Clock:
         self.setLed(currLed-2, brightness=0.50)
         self.setLed(currLed-1, brightness=0.75)
         self.setLed(currLed, brightness=1)
-        self.np.show()
+        self.show()
 
     def ShowColorfade(self, t, fadecolors):
         for i in range(0, self.numLeds):
             self.setLed(i, Colors.PoligonLerp(fadecolors, t) )
         
-        self.np.show()
+        self.show()
 
     def ShowStaticColorfade(self, fadecolors):
         for i in range(0, self.numLeds):
             self.setLed(i, Colors.PoligonLerp(fadecolors, i/(self.numLeds+1)))
-        self.np.show()
+        self.show()
 
     def setLed(self, ledNum, color=(255,255,255), brightness=1.0):
-        ledNum = self.numLeds - (ledNum%self.numLeds) - 23
-        self.np[ledNum] = tuple([round(self.brightness*brightness*x) for x in color])
-   
+        ledNum = ledNum%self.numLeds
+        self.pixels[ledNum].R = color[0]
+        self.pixels[ledNum].G = color[1]
+        self.pixels[ledNum].B = color[2]
+        self.pixels[ledNum].A = brightness
+
+    def show(self):
+        for i in range(0, self.numLeds):
+            ledNum = self.numLeds - (i%self.numLeds) - 23
+            hue = (self.pixels[i].R, self.pixels[i].G, self.pixels[i].B)
+            self.np[ledNum] = tuple([round(self.brightness*self.pixels[i].A*x) for x in hue])
+
+        self.np.show()
+
+
+class clPixel:
+    def __init__(self):
+        self.R = 0
+        self.G = 0
+        self.B = 0
+        self.A = 0.0
--- src/main.py
+++ src/main.py
@@ -2,16 +2,49 @@
 from microbit import *
 from Tijd import *
 from Clock import *
+import music
+import radio
 
 cl = Clock(24, 0.05)
+lt = 0.0
+
+colorScheme = [(0,137,133),(239,121,17)]
+
+state = 0 #0: lost Energy, 1: calibrated, 2: settings
+subState = 0
+
+radio.config(group=177)
+radio.on()
+
 # Code in a 'while True:' loop repeats forever
 while True:
-    #cl.ShowProgressBar(LoopTimeFraction(3000), Truecolor, Falsecolor)
-    #cl.ShowRadarfade(LoopTimeFraction(3000), Truecolor, Falsecolor)
-    #cl.ShowColorfade(LoopTimeFraction(3000), [(255,0,0),(0,255,0),(0,0,255)])
-    #cl.ShowStaticColorfade([(255,0,0),(0,255,0),(0,0,255),(255,0,0),(0,255,0),(0,0,255)])
-    #cl.ShowStaticColorfade([(0,255,0)])
-    cl.RandomLeds()
-    sleep(1000)
+    message = radio.receive()
+    if message:
+        display.scroll(message)
+    
+    if (state == 0):
+        music.play(music.BA_DING)
+        state = 2
+    elif(state == 1):
+        if(subState == 0):
+            cl.ShowProgressBar(LoopTimeFraction(3000), colorScheme[0], colorScheme[1])
+        elif(subState == 1):
+            cl.ShowRadarfade(LoopTimeFraction(3000), colorScheme[0], colorScheme[1])
+        elif(subState == 2):
+            cl.ShowColorfade(LoopTimeFraction(3000), [(255,0,0),(0,255,0),(0,0,255)])
+        elif(subState == 3):
+            cl.ShowStaticColorfade([(255,0,0),(0,255,0),(0,0,255),(255,0,0),(0,255,0),(0,0,255)])
+        elif(subState == 4):
+            cl.ShowStaticColorfade([(0,255,0)])
+        elif(subState == 5):
+            cl.RandomLeds(LoopTimeFraction(250), 0.99)
+        else:
+            cl.ShowStaticColorfade([(255,0,0)])
+    elif(state == 2):
+        if(subState == 0):
+            cl.ShowStaticColorfade([colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[0],colorScheme[1],colorScheme[1],colorScheme[1],colorScheme[1],colorScheme[1],colorScheme[1]])
+        elif(subState == 1):
+            pass
+            
 
         
